6903578b5b968619b2c7a04b4be6473d11c89767
Tatiana Rodionova <tatiana.rodionova@emlid.com>
2020-08-06 23:31:49 +0300

    add TapTargetView implementation to activity

--- app/src/main/java/com/tatiana/rodionova/wish/MainActivity.kt
+++ app/src/main/java/com/tatiana/rodionova/wish/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.tatiana.rodionova.wish
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.getkeepsafe.taptargetview.TapTarget
+import com.getkeepsafe.taptargetview.TapTargetView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +17,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        TapTargetView.showFor(
+            this,
+            TapTarget.forView(
+                findViewById<FloatingActionButton>(R.id.add),
+                "Click to add first wish"
+            )
+                .outerCircleColor(R.color.colorPrimary)
+                .outerCircleAlpha(0.9f)
+                .targetCircleColor(R.color.white)
+                .titleTextSize(20)
+                .titleTextColor(R.color.white)
+                .drawShadow(true)
+                .cancelable(true)
+                .tintTarget(true)
+                .transparentTarget(true)
+                .targetRadius(60),
+            object : TapTargetView.Listener() {
+                override fun onTargetClick(view: TapTargetView?) {
+                    super.onTargetClick(view)
+                    add.performClick()
+                }
+            }
+        )
+        findViewById<FloatingActionButton>(R.id.add).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
